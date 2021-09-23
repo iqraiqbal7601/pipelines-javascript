19618deb16a330bf767f54029b281e98c2313072
iqraiqbal7601 <82566213+iqraiqbal7601@users.noreply.github.com>
2021-09-24 01:12:10 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Arslaan sir!!\n');
+  res.send('Hello Iqra Iqbal!!\n');
 });
 
 var port = process.env.PORT||PORT;
